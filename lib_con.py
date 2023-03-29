57e9f27e05d9eae13b7a9dfaf34fdd4bb04558a5
Kaleb Maravilla <=>
2023-03-28 18:12:48 -0600

    got sorted_by_lines done with the average

--- lib_con.py
+++ lib_con.py
@@ -1,31 +1,43 @@
 import csv
 
-def create_books(file_name):
+def read_books(file_name, book_name):
     '''takes a file and sorts it into different books (tuples)'''
-    woo_list = []
-    ttl_list = []
-    alg_list = []
+    book_list = []
+    
     with open(file_name, encoding='utf8') as file:
         reader = csv.reader(file, delimiter = '|')
         for row in reader:
-            if row[2] == 'WOO':
-                woo_list.append((row[0], row[1]))
-            elif row[2] == 'TTL':
-                ttl_list.append((row[0], row[1]))
-            else:
-                alg_list.append((row[0], row[1]))
-        return dict(woo_list), dict(ttl_list), dict(alg_list)
+            if row[2] == book_name:
+                book_list.append((row[0], int(row[1])))
+            
+        return book_list
 
 def sort_by_lines(my_book):
     '''Takes a book and sorts it in ascending order by the line number'''
-    my_dictionary = dict(my_book)
-    my_values = list(my_dictionary.values())
-    sorted_val = sorted(my_values)
-    #sorted_book = [(line, num) for line, num in sorted(my_book, key = lambda x: x[1])]
-    #sorted_book = dict(sorted(my_book, key= lambda x: (x[0], x[1])))
-    return sorted_val
-        
-books = create_books('data.txt')
-finished_book = sort_by_lines(books[0])
-print(finished_book)
+    sorted_book = [(line, num) for line, num in sorted(my_book, key = lambda x: x[1])]
+    return sorted_book
 
+def min_length(list):
+    '''this function looks at the lines of the book and tells which line is the shortest'''
+    minimum_count = 0
+    minimum_sentence = ''
+    avg_count = []
+    for row in list:
+        min_count = len(row[0])
+        avg_count.append(min_count)
+        if minimum_count == 0:
+            minimum_count = min_count
+            minimum_sentence = row[0]
+        elif min_count < minimum_count:
+            minimum_count = min_count
+            minimum_sentence = row[0]
+    total_avg = int(sum(avg_count) / len(avg_count))
+    return minimum_count, minimum_sentence, total_avg
+
+ttl_book = read_books('data.txt', 'TTL')
+woo_book = read_books('data.txt', 'WOO')
+alg_book = read_books('data.txt', 'ALG')
+
+answer = sort_by_lines(woo_book)
+short_answer = min_length(answer)
+print(short_answer)
