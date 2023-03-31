c39f40e7de02327c7809f90056dd54f6e5891395
Kaleb Maravilla <=>
2023-03-31 08:56:59 -0600

    made things easier to read

--- hw.py
+++ hw.py
@@ -22,3 +22,35 @@
     return "Cannot swap the keys and values for this dictionary"'''
 
 #problem 4 answer
+'''values = (dict, list, set, tuple)
+
+def is_nested(dictionary):
+  my_values = list(dictionary.values())
+  for i in my_values:
+    type_value = type(i)
+    if type_value == tuple or type_value == dict or type_value == list:
+      return True 
+  return(False)
+'''
+
+#problem 5 answer
+import json
+def compare(file1, file2):
+  file1_length = 0
+  file2_length = 0
+
+  with open(file1, 'r') as file, open(file2, 'r') as f:
+    my_dict1 = json.load(file)
+    my_dict2 = json.load(f)
+    file1_length = len(my_dict1)
+    file2_length = len(my_dict2)
+
+
+  if my_dict1 == my_dict2:
+    return 'The dictionaries are equal'
+  elif file1_length > file2_length:
+    return 'Dictionary 1 is longer than dictionary 2'
+  elif file1_length < file2_length:
+    return 'Dictionary 2 is longer than dictionary 1'
+  elif file1_length == file2_length:
+    return 'Dictionary 1 and dictionary 2 have the same length'
--- lib_con.py
+++ lib_con.py
@@ -1,3 +1,4 @@
+
 import csv
 
 def read_books(file_name, book_name):
@@ -24,28 +25,42 @@ def min_max_avg_length(my_list):
     maximum_count = 0
     maximum_sentence = ''
     avg_count = []
+
     for row in my_list:
         min_count = len(row[0])
         max_count = len(row[0])
         avg_count.append(min_count)
         avg_count.append(max_count)
+
         if minimum_count == 0:
             minimum_count = min_count
             maximum_count = max_count
-            minimum_sentence = row[0]
-            maximum_sentence = row[0]
+            minimum_sentence = row[0], row[1]
+            maximum_sentence = row[0], row[1]
+
         elif min_count < minimum_count:
             minimum_count = min_count
-            minimum_sentence = row[0]
+            minimum_sentence = row[0], row[1]
+
         elif max_count > maximum_count:
             maximum_count = max_count
-            maximum_sentence = row[0]
+            maximum_sentence = row[0],row[1]
+
     total_avg = int(sum(avg_count) / len(avg_count))
-    with open("summary_novel.txt", "a") as file:
-        file.write('largest line: {}'.format(maximum_sentence))
-    return minimum_count, minimum_sentence, maximum_count, maximum_sentence, total_avg
+    return minimum_sentence, maximum_sentence, total_avg
 
 
+
+def write_summary_to_file(file_name, file_name2, summary, novel):
+    '''writes the novel summary to a file'''
+    with open(file_name, 'w', encoding = 'utf8') as f, open(file_name2,'w', encoding = 'utf8') as file:
+        #summary file writing
+        f.write('Longest line ({}) : {}\n'.format(summary[1][1], summary[1][0]))
+        f.write('Shortest line ({}): {}\n'.format(summary[0][1], summary[0][0]))
+        f.write('Average length : {}'.format(summary[2]))
+
+        #novel file writing
+        file.write()
 
 
 ttl_book = read_books('data.txt', 'TTL')
@@ -54,4 +69,4 @@ alg_book = read_books('data.txt', 'ALG')
 
 answer = sort_by_lines(woo_book)
 short_answer = min_max_avg_length(answer)
-print(short_answer)
+write_summary_to_file('summary_novel.txt', short_answer)
